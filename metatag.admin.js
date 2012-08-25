2e38db462d4fd4abbb68de06e35cae7fc94446cd
andremolnar <andremolnar@11015.no-reply.drupal.org>
2012-08-25 00:01:46 -0400

    Issue #1327614 by andremolnar: Make arrows clickable on the main settings tree.

--- metatag.admin.js
+++ metatag.admin.js
@@ -5,8 +5,16 @@ Drupal.behaviors.metatagUIConfigListing = {
     // Hide elements to be visible if JavaScript is enabled.
     $('.js-show').show();
 
+    // Make the leaf arrow clickable.
+    $('.metatag-config-label').hover(function(){
+      $(this).css({'cursor':'pointer'});
+    })
+    .click(function(){
+      $(this).find('a.toggle-details', context).trigger('click');
+    });
+
     // Show or hide the summary
-    $('table.metatag-config-overview a.toggle-details', context).click(function() {
+    $('table.metatag-config-overview a.toggle-details', context).click(function(event) {
       $(this).parent('div').siblings('div.metatag-config-details').each(function() {
         if ($(this).hasClass('js-hide')) {
           $(this).slideDown('slow').removeClass('js-hide');
@@ -23,6 +31,10 @@ Drupal.behaviors.metatagUIConfigListing = {
       else {
         $(this).parent('div').removeClass('expanded').addClass('collapsed');
       }
+
+      // This event may be triggered by a parent element click - so we don't
+      // want the click to bubble up otherwise we get recursive click events.
+      event.stopPropagation();
     });
   }
 }
