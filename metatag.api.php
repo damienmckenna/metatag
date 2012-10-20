c8f51ad94955f268af5d439d2204a8bf88fa4ae0
Damien McKenna <damien@mc-kenna.com>
2012-10-19 21:42:48 -0400

    Issue #1817984 by DamienMcKenna, alexweber: Documented hook_metatag_metatags_view_alter().

--- metatag.api.php
+++ metatag.api.php
@@ -3,3 +3,21 @@
  * @file
  * API documentation for the Metatag module.
  */
+
+/**
+ * Alter metatags before being cached.
+ *
+ * This hook is invoked prior to the meta tags for a given page are cached.
+ *
+ * @param array $output
+ *   All of the meta tags to be output for this page in their raw format. This
+ *   is a heavily nested array.
+ * @param string $instance
+ *   An identifier for the current page's page type, typically a combination
+ *   of the entity name and bundle name, e.g. "node:story".
+ */
+function hook_metatag_metatags_view_alter(&$output, $instance) {
+  if (isset($output['description']['#attached']['drupal_add_html_head'][0][0]['#value'])) {
+    $output['description']['#attached']['drupal_add_html_head'][0][0]['#value'] = 'O rly?';
+  }
+}
