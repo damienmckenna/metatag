686f3624d629c41465973399b46d28c62002e094
fjgarlin <fjgarlin@2495842.no-reply.drupal.org>
2017-05-08 13:04:13 -0400

    Issue #2857544 by fjgarlin: Default values not returned on REST default routes.

---
+++ src/Tests/MetatagJsonOutputTest.php
@@ -0,0 +1,27 @@
+<?php
+// @todo Write this
+
+namespace Drupal\metatag\Tests;
+
+use Drupal\simpletest\WebTestBase;
+
+/**
+ * Ensures that meta tag values can be output using JSON.
+ *
+ * @group metatag
+ */
+// class MetatagJsonOutputTest extends WebTestBase {
+//
+//   /**
+//    * Modules to enable.
+//    *
+//    * @var array
+//    */
+//   public static $modules = [
+//     'content_translation',
+//     'field_ui',
+//     'metatag',
+//     'node',
+//   ];
+//
+// }
