e8c06720dddbcb1053239790aa7ddb7529413444
kanei <kanei@803868.no-reply.drupal.org>
2018-06-03 22:08:05 -0400

    Issue #2973277 by mvantuch, DamienMcKenna: Support for og:image:alt.

--- metatag_open_graph/src/Tests/MetatagOpenGraphTagsTest.php
+++ metatag_open_graph/src/Tests/MetatagOpenGraphTagsTest.php
@@ -32,6 +32,7 @@ class MetatagOpenGraphTagsTest extends MetatagTagsTestBase {
     'og_email',
     'og_fax_number',
     'og_image',
+    'og_image_alt',
     'og_image_height',
     'og_image_secure_url',
     'og_image_type',
