cc490928b47c586470959313b420430c300584e9
Caffeinated <Caffeinated@2507260.no-reply.drupal.org>
2018-06-21 10:54:16 -0400

    Issue #2961918 by thejimbirch, Michelle: Add the site.webmanifest meta tag.

--- metatag_mobile/src/Tests/MetatagMobileTagsTest.php
+++ metatag_mobile/src/Tests/MetatagMobileTagsTest.php
@@ -46,6 +46,7 @@ class MetatagMobileTagsTest extends MetatagTagsTestBase {
     'msapplication_window',
     'theme_color',
     'viewport',
+    'web_manifest',
     'x_ua_compatible',
   ];
 
@@ -210,6 +211,20 @@ class MetatagMobileTagsTest extends MetatagTagsTestBase {
    */
   private function msapplicationWide310x150logoTestValue() {
     return $this->randomImageUrl();
+  }
+
+  /**
+   * Implements {tag_name}TestOutputXpath() for 'web_manifest'.
+   */
+  private function webManifestTestOutputXpath() {
+    return "//link[@rel='manifest']";
+  }
+
+  /**
+   * Implements {tag_name}TestValueAttribute() for 'web_manifest'.
+   */
+  private function webManifestTestValueAttribute() {
+    return 'href';
   }
 
   /**
