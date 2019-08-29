99b9dcc14b4b08274b014a8506132b5a4a655bbf
david.pashaev <david.pashaev@3487675.no-reply.drupal.org>
2019-08-29 11:13:41 -0400

    Issue #3077784 by davps: Fix compatibility / tests against core 8.8.x

--- tests/src/Kernel/MetatagSettingsTest.php
+++ tests/src/Kernel/MetatagSettingsTest.php
@@ -14,22 +14,49 @@ class MetatagSettingsTest extends KernelTestBase {
   /**
    * {@inheritdoc}
    */
-  public static $modules = [
-    'metatag',
-    'user',
-  ];
+  public static $modules = ['metatag'];
+
+  /**
+   * The metatag manager.
+   *
+   * @var \Drupal\metatag\MetatagManagerInterface
+   */
+  protected $metatagManager;
+
+  /**
+   * The configuration factory service.
+   *
+   * @var \Drupal\Core\Config\ConfigFactoryInterface
+   */
+  protected $configFactory;
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function setUp() {
+    parent::setUp();
+
+    $this->metatagManager = $this->container->get('metatag.manager');
+    $this->configFactory = $this->container->get('config.factory');
+
+    $this->installConfig(['metatag']);
+  }
 
   /**
    * Tests the Metatag settings.
    */
   public function testMetatagSettings() {
-    $metatag_groups = \Drupal::service('metatag.manager')->sortedGroups();
+    $metatag_groups = $this->metatagManager->sortedGroups();
+    $config = $this->configFactory->getEditable('metatag.settings');
+
     $group = reset($metatag_groups);
     $group_id = $group['id'];
-    $config = \Drupal::configFactory()->getEditable('metatag.settings');
+
     $value = [];
     $value['user']['user'][$group_id] = $group_id;
     $config->set('entity_type_groups', $value)->save();
+
+    $this->assertSame($value, $config->get('entity_type_groups'));
   }
 
 }
